0404061d3e60efccd304246c55f5603d042f320f
Matt Arnold <matt.jahnold@googlemail.com>
2015-03-16 20:46:50 +0000

    Playback Fragment

--- app/src/main/java/com/jahnold/syncaudiobookplayer/Fragments/BookListFragment.java
+++ app/src/main/java/com/jahnold/syncaudiobookplayer/Fragments/BookListFragment.java
@@ -2,9 +2,11 @@ package com.jahnold.syncaudiobookplayer.Fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.jahnold.syncaudiobookplayer.Adapters.BookAdapter;
@@ -19,7 +21,7 @@ import java.util.List;
 /**
  *  Fragment which lists all books for user
  */
-public class BookListFragment extends android.support.v4.app.Fragment {
+public class BookListFragment extends Fragment {
 
     private BookAdapter mAdapter;
     private ArrayList<Book> mBooks = new ArrayList<>();
@@ -42,6 +44,25 @@ public class BookListFragment extends android.support.v4.app.Fragment {
                 mBooks
         );
         bookList.setAdapter(mAdapter);
+
+        // set up the click listener for the list items (books)
+        bookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // get the clicked book
+                Book book = mAdapter.getItem(position);
+
+                // switch to the playback fragment
+                PlaybackFragment playbackFragment = new PlaybackFragment();
+                playbackFragment.setBook(book);
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, playbackFragment, "PlaybackFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
 
         // get all the books
         Book.loadAll(new FindCallback<Book>() {
--- app/src/main/java/com/jahnold/syncaudiobookplayer/Fragments/PlaybackFragment.java
+++ app/src/main/java/com/jahnold/syncaudiobookplayer/Fragments/PlaybackFragment.java
@@ -1,7 +1,105 @@
 package com.jahnold.syncaudiobookplayer.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.jahnold.syncaudiobookplayer.Models.Book;
+import com.jahnold.syncaudiobookplayer.R;
+
 /**
- * Created by matthewarnold on 05/03/15.
+ *  Playback Fragment
  */
-public class PlaybackFragment {
+public class PlaybackFragment extends Fragment implements View.OnClickListener {
+
+    private Book mBook;
+
+    // empty constructor
+    public PlaybackFragment() {}
+
+    // setters
+    public void setBook(Book book) { mBook = book; }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_playback,container,false);
+
+        // get refs to controls
+        ImageView imgCover = (ImageView) v.findViewById(R.id.img_cover);
+        ImageButton btnBack = (ImageButton) v.findViewById(R.id.btn_back);
+        ImageButton btnPlayPause = (ImageButton) v.findViewById(R.id.btn_play_pause);
+        ImageButton btnSpecialPause = (ImageButton) v.findViewById(R.id.btn_special_pause);
+        ImageButton btnForward = (ImageButton) v.findViewById(R.id.btn_forward);
+        TextView txtTitle = (TextView) v.findViewById(R.id.txt_title);
+        TextView txtAuthor = (TextView) v.findViewById(R.id.txt_author);
+        ProgressBar progressBar = (ProgressBar) v.findViewById(R.id.progress_bar);
+
+
+        // set things from the book
+        if (mBook != null) {
+
+            txtTitle.setText(mBook.getTitle());
+            txtAuthor.setText(mBook.getAuthor());
+
+            progressBar.setMax(mBook.getLength());
+            progressBar.setProgress(mBook.getCurrentPosition());
+
+        }
+
+        // set the click listeners
+        btnBack.setOnClickListener(this);
+        btnForward.setOnClickListener(this);
+        btnPlayPause.setOnClickListener(this);
+        btnSpecialPause.setOnClickListener(this);
+
+        return v;
+    }
+
+    /**
+    *   Route the onClick events to the appropriate method
+    */
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_back:
+                onBackClick();
+                break;
+            case R.id.btn_forward:
+                onForwardClick();
+                break;
+            case R.id.btn_play_pause:
+                onPlayPauseClick();
+                break;
+            case R.id.btn_special_pause:
+                onSpecialPauseClick();
+                break;
+        }
+    }
+
+    public void onPlayPauseClick() {
+
+    }
+
+    public void onBackClick() {
+
+    }
+
+    public void onSpecialPauseClick() {
+
+    }
+
+    public void onForwardClick() {
+
+    }
+
+
 }
